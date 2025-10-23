1c3a8b4beab97ccec44f0d67de51338d13529378
Kamran <kamran@afsharian.net>
2025-10-23 03:20:47 -0400

    commandqueue

--- include/engproj/data_structures/command_queue.hpp
+++ include/engproj/data_structures/command_queue.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cassert>
+#include <condition_variable>
 #include <mutex>
 #include <vector>
 namespace engproj::data_structures{
@@ -17,24 +18,53 @@ public:
   command_queue(command_queue&&) = delete;
   command_queue& operator=(command_queue&&) = delete;
 
-  void push(command_type& command){
-    write_buffer_.emplace_back(std::move(command));
+/*
+  template<typename U>
+  void push(U&& command){
+    std::scoped_lock lock(mtx_);
+    write_buffer_.emplace_back(std::forward<U>(command));
   }
-
-  void push_commands(std::vector<command_type>& commands){//consumes vector
+*/
+  template<typename Vec>
+  void push_commands(Vec&& commands){//consumes vector
     std::scoped_lock lock(mtx_);
     write_buffer_.reserve(write_buffer_.size() + commands.size());
-    std::move(commands.begin(),commands.end(), std::back_inserter(write_buffer_));
-    commands.clear();
+    if constexpr (std::is_rvalue_reference_v<decltype(commands)>){
+      std::move(std::begin(commands),std::end(commands), std::back_inserter(write_buffer_));
+    }else{
+      std::copy(std::begin(commands),std::end(commands), std::back_inserter(write_buffer_));
+    }
   }
 
-  std::vector<command_type> extract_commands_and_swap(){
-    //single consumer no lock neccessary, must be called by consumer after producers finish
-    assert("no producers can be writing here" && !mtx_.try_lock());
+  void register_thread(){
+    std::scoped_lock lock(mtx_);
+    thread_count_++;
+    cv_.notify_one();
+  }
+
+  void deregister_thread(){
+    std::scoped_lock lock(mtx_);
+    thread_count_--;
+    if(thread_count_ < 0){
+      thread_count_ = 0;
+    }
+    cv_.notify_one();
+  }
+
+  void signal_thread(){
+    std::scoped_lock lock(mtx_);
+    signaled_count_++;
+    cv_.notify_one();
+  }
+
+  std::vector<command_type> extract_commands_and_swap(){//there might be a problem with buffer capacities getting to big
+    std::scoped_lock lock(mtx_);
+    cv_.wait(lock,[&]{return thread_count_<=signaled_count_;});//only one consumer should ever be waiting here
+    signaled_count_=0;
     auto temp = std::move(read_buffer_);
     std::swap(write_buffer_,read_buffer_);
-    write_buffer_.clear();
-    return std::move(temp);
+    clear();
+    return temp;
   }
 
   void clear(){
@@ -48,8 +78,9 @@ private:
   std::vector<command_type> write_buffer_;
   std::vector<command_type> read_buffer_;
   std::mutex mtx_;
-
-
+  std::condition_variable cv_;
+  std::size_t thread_count_ = 0;
+  std::size_t signaled_count_ = 0;
 
 };
 
